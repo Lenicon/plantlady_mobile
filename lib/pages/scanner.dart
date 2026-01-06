db255d391b43139c5add62d565966a09a31fa845
Len.icon <lenthesmiledev@gmail.com>
2026-01-06 23:12:32 +0800

    add appbar to scanner

--- lib/pages/scanner.dart
+++ lib/pages/scanner.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
 
-class Scanner extends StatelessWidget {
+class Scanner extends StatefulWidget {
   const Scanner({super.key});
 
   @override
+  State<Scanner> createState() => _ScannerState();
+}
+
+class _ScannerState extends State<Scanner> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        'Plant Scanner',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 24,
+          fontWeight: FontWeight.w900,
+        ),
+      ),
+      // backgroundColor: Color.fromARGB(255, 247, 220, 238),
+      elevation: 0.0,
+      // shadowColor: Colors.pink,
+    );
   }
 }
